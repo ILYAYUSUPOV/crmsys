954b91b173e2bcf2243c9d33281de74adc97b9cb
Ilya <ilyayusupovilya@gmail.com>
2020-09-29 14:46:25 +0300

    start git

---
+++ src/components/HomeComponents/Header.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="Header">
+    <div class="burgerMenu">
+      <h1 class="logo">CRMsystem</h1>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,21 @@
 <template>
   <div>
+    <Header></Header>
   </div>
 </template>
 
 <script>
+import Header from '@/components/HomeComponents/Header'
+
 export default {
   name: 'Home',
+
+  components: {
+    Header
+  },
 }
 </script>
+
+<style scoped>
+
+</style>
